dd22f1358c3c01382c0b9baa92876b24c9bc5040
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-04-16 13:52:51 +0300

    Commit 8 16.04.21 Create UserDaoImpl.java.

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,47 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUser() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User findById(Long id) {
+        TypedQuery<User> query = entityManager.createQuery(
+                "select u from User u where u.id = :id", User.class);
+        query.setParameter("id", id);
+        return query.getResultList().stream().findAny().orElse(null);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        TypedQuery<User> query = entityManager.createQuery(
+                "select u from User u where u.id = :id", User.class);
+        query.setParameter("id", id);
+        User user = query.getResultList().stream().findAny().orElse(null);
+        entityManager.remove(user);
+    }
+
