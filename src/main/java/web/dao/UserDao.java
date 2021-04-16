49117d6cfe5f87072b37379e451398759c21e490
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-04-16 13:50:42 +0300

    Commit 7 16.04.21 Change UserController.java and create UserDao.java.

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.dao;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> getAllUser();
+
+    User findById(Long id);
+
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteById(Long id);
+}
+
