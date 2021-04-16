339512b3f841a995e6c14a9e7c1e6089b4e66a53
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-04-16 14:03:26 +0300

    Commit 10 16.04.21 Create UserService.java, UserServiceImpl.java.

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import web.model.User;
+import web.service.UserService;
 
 import java.util.List;
 
--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,10 +1,10 @@
-package web.dao;
+package web.service;
 
 import web.model.User;
 
 import java.util.List;
 
-public interface UserDao {
+public interface UserService {
 
     List<User> getAllUser();
 
